5a05eac8aaeb3b50e0a9bd82df41c08f87448f6e
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-11-21 13:27:46 -0600

    completed

--- Student.cs
+++ Student.cs
@@ -1,13 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-namespace studentList
+namespace StudentExercises
 {
     class Student
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string SlackHandle { get; set; }
-        public string StudentCohort { get; set; }
-        public string Exercises { get; set; }
+        public int Cohort { get; set; }
+
+        public List<Exercise> Exercises = new List<Exercise>();
+
+        // Constructor
+
+        public Student(string firstName, string lastName, string slackHandle, int cohort)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SlackHandle = slackHandle;
+            Cohort = cohort;
+        }
     }
 }
