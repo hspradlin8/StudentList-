e4a6826da1356a6d038ff70234ac2f9eb9c8680c
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-11-22 09:56:26 -0600

    part2

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ namespace StudentExercises
             var designClassWebsite = new Exercise("Design Class Website", "UI/UX");
             var planetsAndSpaceship = new Exercise("Planets And Spacespaces", "C Sharp");
             var dictionaryOfWords = new Exercise("Dictionary of Words", "C Sharp");
+            var jsArrays = new Exercise("Javascript Arrays", "Javascript");
+            var jsObjects = new Exercise("Javascript Objects", "Javascript");
 
             // Cohorts
             var cohort35 = new Cohort("Cohort 35");
@@ -32,9 +34,9 @@ namespace StudentExercises
 
             // Add Students To Cohort
             cohort35.Students.Add(nickWessel);
-            cohort35.Students.Add(dyalnGriffith);
+            cohort37.Students.Add(dyalnGriffith);
             cohort35.Students.Add(davidCornfish);
-            cohort35.Students.Add(sageKlein);
+            cohort36.Students.Add(sageKlein);
 
             // Instructors Assigning Exercises
             brendaLong.assignExercise(cohort35.Students, designClassWebsite);
@@ -56,7 +58,6 @@ namespace StudentExercises
             {
                 Console.WriteLine("");
                 Console.WriteLine(exercise.Name);
-                Console.WriteLine("---------------");
                 foreach (Student student in cohort35.Students)
                 {
                     if (student.Exercises.Contains(exercise))
@@ -65,6 +66,107 @@ namespace StudentExercises
                     }
                 }
             }
+
+            List<Student> students = new List<Student>()
+            {
+                nickWessel,
+                dyalnGriffith,
+                davidCornfish,
+                sageKlein,
+            };
+
+            List<Exercise> exercises = new List<Exercise>()
+            {
+                planYourHeist,
+                designClassWebsite,
+                planetsAndSpaceship,
+                dictionaryOfWords,
+                jsArrays,
+                jsObjects,
+            };
+
+            List<Cohort> cohorts = new List<Cohort>()
+            {
+                cohort35,
+                cohort36,
+                cohort37,
+            };
+
+            List<Instructor> instructors = new List<Instructor>()
+            {
+                madisonPepper,
+                adamSheaffer,
+                brendaLong,
+            };
+
+            // List exercises for the JavaScript language by using the Where() LINQ method.
+            var jsExercises = (from exercise in exercises where exercise.Language == "Javascript"
+                select exercise).ToList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Javascript Exercises:");
+            Console.WriteLine("-------");
+            jsExercises.ForEach(exercise =>
+            {
+                Console.WriteLine(exercise.Name);
+            });
+
+            // List students in a particular cohort by using the Where() LINQ method.
+
+            var cohort35Students = (from student in students where student.Cohort == 35 select student).ToList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Cohort 35 Students");
+            Console.WriteLine("-------");
+            cohort35Students.ForEach(student =>
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            });
+
+            // List instructors in a particular cohort by using the Where() LINQ method.
+
+            var cohort35Instructors = (from instructor in instructors where instructor.Cohort == 35 select instructor).ToList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Cohort 35 Instructors");
+            Console.WriteLine("-------");
+            cohort35Instructors.ForEach(instructor =>
+            {
+                Console.WriteLine($"{instructor.FirstName} {instructor.LastName}");
+            });
+
+            // Sort the students by their last name.
+
+            List<Student> orderedLastNameStudents = students.OrderBy(student => student.LastName).ToList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Students by last name:");
+            Console.WriteLine("-------");
+            orderedLastNameStudents.ForEach(student =>
+                Console.WriteLine($"{student.FirstName} {student.LastName}"));
+
+            // Display any students that aren't working on any exercises 
+            // (Make sure one of your student instances don't have any exercises. 
+            // Create a new student if you need to.)
+            List<Student> noStudentExercises = (from student in students where student.Exercises.Count == 0 select student).ToList();
+            Console.WriteLine("");
+            Console.WriteLine("This student is not working on any exercises");
+            Console.WriteLine("-------");
+            noStudentExercises.ForEach(student => Console.WriteLine($"{student.FirstName} {student.LastName}"));
+
+            // Which student is working on the most exercises? Make sure one of your students has more exercises than the others.
+            List<Student> mostStudentExercises = (from student in students orderby student.Exercises.Count descending select student).ToList();
+
+            Student mostExercises = mostStudentExercises.First();
+            Console.WriteLine($"Student working on most exercises: {mostExercises.FirstName} {mostExercises.LastName}");
+            Console.WriteLine("-------");
+
+            // How many students in each cohort?
+            Console.WriteLine("");
+            Console.WriteLine("Students in each Cohort:");
+            Console.WriteLine("-------");
+            cohorts.ForEach(cohort => Console.WriteLine($"{cohort.Name} - {cohort.Students.Count}"));
+
         }
     }
 }
